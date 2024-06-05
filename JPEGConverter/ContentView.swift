c6d8fa6a2bbe0026feb54a8a336ee21725b71d01
유인호 <yuiop1029@naver.com>
2024-06-05 17:16:43 +0900

    Initial Commit

---
+++ JPEGConverter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JPEGConverter
+//
+//  Created by A_Mcflurry on 6/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JPEGConverter/JPEGConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPEGConverterApp.swift
+//  JPEGConverter
+//
+//  Created by A_Mcflurry on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct JPEGConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
